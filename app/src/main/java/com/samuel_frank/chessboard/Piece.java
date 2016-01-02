1e4aecab1fce9a3324c918e9d2fecfc805db03c9
Sam Frank <sfrank2147@gmail.com>
2016-01-02 14:59:08 -0500

    Background is now a red square.

---
+++ app/src/main/java/com/samuel_frank/chessboard/Piece.java
@@ -0,0 +1,8 @@
+package com.samuel_frank.chessboard;
+
+/**
+ * Created by smf2147 on 1/2/16.
+ */
+public abstract class Piece {
+    public Piece() {}
+}
---
+++ app/src/main/java/com/samuel_frank/chessboard/Square.java
@@ -0,0 +1,11 @@
+package com.samuel_frank.chessboard;
+
+/**
+ * Created by smf2147 on 1/2/16.
+ */
+public class Square {
+    public Square() {
+        this.piece = null;
+    }
+    private Piece piece;
+}
