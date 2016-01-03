41fd72eb0333caf61a0dae0a84cdb9d7cd3b791c
Sam Frank <sfrank2147@gmail.com>
2016-01-03 13:45:31 -0500

    Chess board is now displaying as a square.

---
+++ app/src/main/java/com/samuel_frank/chessboard/Board.java
@@ -0,0 +1,53 @@
+package com.samuel_frank.chessboard;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * Created by smf2147 on 1/2/16.
+ */
+public class Board extends LinearLayout {
+    private static final String TAG = "Board";
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    private int size;
+
+    private Square[][] squares;
+
+    public Board(Context context) {
+        super(context);
+        initializeSquares(context);
+    }
+
+    public Board(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initializeSquares(context);
+    }
+
+    private void initializeSquares(Context context) {
+        this.squares = new Square[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Square curSquare = new Square(context);
+                curSquare.setBoard(this);
+                this.squares[i][j] = curSquare;
+            }
+        }
+    }
+
+    @Override
+    public void onMeasure(int width, int height) {
+        int newSize = (width < height) ? width : height;
+        super.onMeasure(newSize, newSize);
+    }
+}
--- app/src/main/java/com/samuel_frank/chessboard/Square.java
+++ app/src/main/java/com/samuel_frank/chessboard/Square.java
@@ -1,11 +1,49 @@
 package com.samuel_frank.chessboard;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ImageButton;
+
 /**
  * Created by smf2147 on 1/2/16.
  */
-public class Square {
-    public Square() {
-        this.piece = null;
+public class Square extends ImageButton {
+    private static final String TAG = "Square";
+
+    public Square(Context context) {
+        super(context);
+        this.initializePrivateMembers();
+        Log.d(TAG, "Initialized!");
     }
+
+    public Square(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.initializePrivateMembers();
+        Log.d(TAG, "Initialized!");
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    private void initializePrivateMembers() {
+        this.piece = null;
+        this.board = null;
+    }
+
     private Piece piece;
+    private Board board;
 }
